d4c73b65fd12e029ce7a02b3929b44c84e1e99b0
Michael Klishevich <busation@gmail.com>
2019-01-16 00:04:10 +0300

    handlebars

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require('express');
+const handlebars = require('handlebars');
+const fs = require('fs');
+const path = require('path');
+
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const hbsFileContent = fs.readFileSync(path.join(process.cwd(), 'index.hbs'), 'utf8');
+const template = handlebars.compile(hbsFileContent);
+const context = {
+    title: 'My New App!',
+    body: 'Hello World!'
+};
+
+app.get('/', (req, res) => res.send(template(context)));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
