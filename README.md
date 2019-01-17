7d3b55a643bb82b2258977b3d62c424bcf5b1547
Michael Klishevich <busation@gmail.com>
2019-01-17 09:39:56 +0300

    CRUD create only

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # demo-server-5-min
+
+## Install
+
+1. Установить npm и node.js - https://nodejs.org/en/download/
+2. Скачать проект - `git clone` или скачать архив
+3. Запустить проект `node app.js`
--- app.js
+++ app.js
@@ -2,18 +2,44 @@ const express = require('express');
 const handlebars = require('handlebars');
 const fs = require('fs');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
-app.use(express.static('public'))
+app.use(express.static('public'));
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // to support URL-encoded bodies
+
 const port = 3000;
+
+let cats = [
+    {
+        name: 'Fluffy',
+        age: '2'
+    },
+    {
+        name: 'Smelly',
+        age: '5'
+    }
+];
 
 const hbsFileContent = fs.readFileSync(path.join(process.cwd(), 'index.hbs'), 'utf8');
 const template = handlebars.compile(hbsFileContent);
 const context = {
     title: 'My New App!',
-    body: 'Hello World!'
+    body: 'Hello World!',
+    cats
 };
 
 app.get('/', (req, res) => res.send(template(context)));
+
+app.post('/cats', (req, res) => {
+    const body = req.body;
+    console.log('body', body);
+    cats.push({
+        name: body.catName,
+        age: body.catAge
+    });
+    res.redirect('/');
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
